448e30a6403bdf21d512abc3d920aba18c65ad96
Grant Wells <gswells0206@gmail.com>
2025-03-01 21:40:18 -0600

    Deployed basic home page for Chirper.

--- conf/urls.py
+++ conf/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),  # Django Admin Access
+    path("", include("home.urls")),  # Redirect the root URL to the homepage. --> 'app'
 ]
