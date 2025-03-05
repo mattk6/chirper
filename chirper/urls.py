bb6d5b224d1a990dbb0d597d1c22817f645454a9
Matthew Kruse <kruse.matthew2@gmail.com>
2025-03-05 17:21:17 -0600

    likes work now, but still gotta refresh to see

--- chirper/urls.py
+++ chirper/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('profile/chirp/', views.post_chirp, name='post_chirp'),
+    path('profile/chirp/<int:chirp_id>/like/', views.like_chirp, name='like_chirp'),
 ]
