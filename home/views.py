5395c4782ec5b57374931f5aa3e7ec3a7bb7acf5
Matthew Kruse <kruse.matthew2@gmail.com>
2025-03-04 19:48:43 -0600

    require login to view profile

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, "home/index.html")
+
+
+
+@login_required  # Ensure that the user is logged in before accessing the profile
+def profile_view(request):
+    return render(request, 'profile.html')
